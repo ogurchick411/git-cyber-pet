a6bf39e86a32d7a8db5d75bd1430eff6412b03b8
Ogurchick <148265956+ogurchick411@users.noreply.github.com>
2026-07-15 23:01:41 +0200

    Initialize project structure and create DevBot state

---
+++ test.js
@@ -0,0 +1,14 @@
+let devBot = {
+    energy: 100,
+    bugs: 0,
+    isAlive: true
+};
+
+// Selecting DOM elements [cite: 1, 2]
+const energyVal = document.querySelector("#energy-val"); // [cite: 2]
+const bugsVal = document.querySelector("#bugs-val"); // [cite: 2]
+const statusText = document.querySelector("#status-text"); // [cite: 2]
+
+const chargeBtn = document.querySelector("#charge-btn"); // [cite: 2]
+const debugBtn = document.querySelector("#debug-btn"); // [cite: 2]
+
